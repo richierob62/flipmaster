8e99f0b9ef810c10e1405db973a852b45ff47bfe
richierob62 <richierob62@gmail.com>
2019-07-01 18:32:40 -0400

    Refactor canvas positioning

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const GAME_WIDTH = 600
+const GAME_HEIGHT = 600
+
+export default { GAME_HEIGHT, GAME_WIDTH }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import Constants from './constants'
+
 window.addEventListener('DOMContentLoaded', event => {
   const canvas = document.getElementById('canvas')
   const c = canvas.getContext('2d')
-  c.fillStyle = 'red'
-  c.fillRect(0, 0, 800, 800)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, Constants.GAME_WIDTH, Constants.GAME_HEIGHT)
 })
