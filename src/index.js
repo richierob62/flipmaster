0c52241c2f35e8fe372840f99dfdd99dc2be4167
richierob62 <richierob62@gmail.com>
2019-07-01 17:58:52 -0400

    Draw canvas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('running')
+window.addEventListener('DOMContentLoaded', event => {
+  const canvas = document.getElementById('canvas')
+  const c = canvas.getContext('2d')
+  c.fillStyle = 'red'
+  c.fillRect(100, 100, 400, 300)
+})
