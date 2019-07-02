f56dc8850c7332fb1c5db983b62c652c9e56f5ef
richierob62 <richierob62@gmail.com>
2019-07-01 22:02:39 -0400

    Set canvas size

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import Constants from './constants'
+import Game from './game'
 
 window.addEventListener('DOMContentLoaded', event => {
   const canvas = document.getElementById('canvas')
-  const c = canvas.getContext('2d')
-  c.fillStyle = 'white'
-  c.fillRect(0, 0, Constants.GAME_WIDTH, Constants.GAME_HEIGHT)
+  const game = new Game(canvas)
 })
